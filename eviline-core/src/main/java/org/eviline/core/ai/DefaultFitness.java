47df785dfd03734aea2ce49717608b3d4db272a9
robin <robin@robinkirkman.com>
2014-03-31 11:05:43 -0400

    Slightly saner default fitness function

--- eviline-core/src/main/java/org/eviline/core/ai/DefaultFitness.java
+++ eviline-core/src/main/java/org/eviline/core/ai/DefaultFitness.java
@@ -12,12 +12,20 @@ public class DefaultFitness implements Fitness {
 		int blocksBefore = 0;
 		int blocksAfter = 0;
 		
+		int vtxBefore = 0;
+		int vtxAfter = 0;
+		
 		for(int y = -4; y < Field.HEIGHT; y++) {
 			blocksBefore += Long.bitCount(before.mask(y)) * (20 - y);
 			blocksAfter += Long.bitCount(after.mask(y)) * (20 - y);
 		}
+
+		for(int y = -3; y < Field.HEIGHT; y++) {
+			vtxBefore += Long.bitCount(before.mask(y-1) ^ before.mask(y));
+			vtxAfter += Long.bitCount(after.mask(y-1) ^ after.mask(y));
+		}
 		
-		return blocksAfter - blocksBefore;
+		return (blocksAfter - blocksBefore) + (vtxAfter - vtxBefore) * 30;
 	}
 
 }
