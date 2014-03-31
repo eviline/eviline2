d4e9eefa9dbc2cbcc9da99afc611ee97344b537c
robin <robin@robinkirkman.com>
2014-03-31 09:38:26 -0400

    Fitness which just counts the blocks

---
+++ eviline-core/src/main/java/org/eviline/core/ai/DefaultFitness.java
@@ -0,0 +1,23 @@
+package org.eviline.core.ai;
+
+import org.eviline.core.Field;
+
+public class DefaultFitness implements Fitness {
+
+	@Override
+	public double badness(Field before, Field after) {
+		after = after.clone();
+		after.clearLines();
+		
+		int blocksBefore = 0;
+		int blocksAfter = 0;
+		
+		for(int y = -4; y < Field.HEIGHT; y++) {
+			blocksBefore += Long.bitCount(before.mask(y));
+			blocksAfter += Long.bitCount(after.mask(y));
+		}
+		
+		return blocksAfter - blocksBefore;
+	}
+
+}
