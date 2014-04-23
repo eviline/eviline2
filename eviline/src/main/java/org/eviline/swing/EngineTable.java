8137a749f89b933f731f016661b1a8d5d272b6a9
robin <robin@robinkirkman.com>
2014-04-23 07:04:07 -0400

    Speed up rendering by drawing rectangles rather than individual pixels

--- eviline/src/main/java/org/eviline/swing/EngineTable.java
+++ eviline/src/main/java/org/eviline/swing/EngineTable.java
@@ -63,13 +63,13 @@ public class EngineTable extends JTable {
 				g.drawImage(blockImage.get(next), blockSize, blockSize * 4, null);
 			}
 		}
-		for(int x = 0; x < getWidth(); x++) {
-			for(int y = 0; y < getHeight(); y++) {
+		for(int y = 0; y < getHeight(); y++) {
+//			for(int x = 0; x < getWidth(); x++) {
 				long s = engine.getTickCount() - y*8/blockSize;
 				int w = (int)(15 * (1 + Math.sin(blockSize * s / 144.)));
 				g.setColor(new Color(w, w, w, 164));
-				g.fillRect(x, y, 1, 1);
-			}
+				g.fillRect(0, y, getWidth(), 1);
+//			}
 		}
 		super.paintComponent(g);
 	}
