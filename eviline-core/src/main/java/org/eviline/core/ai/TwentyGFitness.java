2ed0a65c430d9cb3eb313d63e12b04101093ca46
robin <robin.kirkman@gmail.com>
2015-04-06 09:11:38 -0400

    20G fitness

---
+++ eviline-core/src/main/java/org/eviline/core/ai/TwentyGFitness.java
@@ -0,0 +1,13 @@
+package org.eviline.core.ai;
+
+import org.eviline.core.Field;
+import org.eviline.core.ShapeType;
+import org.eviline.core.Shorts;
+
+public class TwentyGFitness extends DefaultFitness {
+	public TwentyGFitness() {
+		c = new double[] {
+				1.9336283987124731, .36005330744432673, 10.98229343443467, 20.862352551247495, 50.02320808076007677
+		};
+	}
+}
