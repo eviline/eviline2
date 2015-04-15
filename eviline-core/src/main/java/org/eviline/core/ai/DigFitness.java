a2cd6ab34cb0d43dd57f16629856e460139460ff
robin <robin.kirkman@gmail.com>
2015-04-15 01:05:16 -0400

    DigFitness, still needs tuning

---
+++ eviline-core/src/main/java/org/eviline/core/ai/DigFitness.java
@@ -0,0 +1,9 @@
+package org.eviline.core.ai;
+
+public class DigFitness extends DefaultFitness {
+	public DigFitness() {
+		super.c = new double[] {
+				0.05336280567794457, 0.23280440406648997, 1.1905581008923116, 7.23786368971029, -0.21619954164453004
+		};
+	}
+}
